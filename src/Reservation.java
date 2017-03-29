745b4361862bbe20b6e37b1081201660578ec1a2
petxa01 <petxa01@gmail.com>
2017-03-29 13:26:03 +0200

    remove the no necesary imports

--- src/Reservation.java
+++ src/Reservation.java
@@ -6,9 +6,7 @@
 
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
